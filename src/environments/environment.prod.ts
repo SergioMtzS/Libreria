fa40560cfc7d3e0f2c8c46068aa4f4782c974d9a
Sergio <sergiopsc2@gmail.com>
2021-12-08 17:54:36 -0600

    Vercel

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  dialogflow: { 
+    angularBot: '83ef8292f8a24c9b14df34751e497043651f0f97'
+},
+firebase: {
+  apiKey: 'AIzaSyBicowszoZIpChqlGWgTucENkYFZMgozG8',
+  authDomain: 'dwi9a-40583.firebaseapp.com',
+  databaseURL: '<your-database-url>',
+  projectId: 'dwi9a-40583',
+  storageBucket: 'gs://dwi9a-40583.appspot.com',
+  messagingSenderId: '932182395719'
+}
+};
